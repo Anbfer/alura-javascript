5f78b09bed91bb0fcabaca185420a1010a6c2939
Ângelo Baracho Ferreira <92029180+Anbfer@users.noreply.github.com>
2021-10-14 09:12:31 -0300

    Update filtra.js
    finalizando filtro, adicionando classe "Invisível" para esconder o conteúdo filtrado.

--- docs/js/filtra.js
+++ docs/js/filtra.js
@@ -9,7 +9,13 @@ campoFiltro.addEventListener("input", function(){
             for (var i = 0; i < pacientes.length; i++ ){
                     var paciente = pacientes [i];
                     var tdNome = paciente.querySelector(".info-nome");
-                    var nome = 
+                    var nome = tdNome.textContent;
+
+                    if (nome != this.value){
+                        paciente.classList.add(".invisivel");
+                    } else {
+                        paciente.classList.remove(".invisivel");
+                    }
             }
         }
 });
